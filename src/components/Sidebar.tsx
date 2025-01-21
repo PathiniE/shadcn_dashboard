be7e95de395ea5856cbc2dad13ffa20af1b2d90c
Pathini Ekanayaka <pathini2002@gmail.com>
2025-01-21 12:02:33 +0530

    add side bar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,83 @@
+'use client';
+
+import { group } from "console";
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList, CommandSeparator } from "./ui/command";
+import Useritem from "./Useritem";
+import { text } from "stream/consumers";
+import { User,Inbox,Receipt,Bell,Settings,Cookie,MessageSquare } from "lucide-react";
+
+export default function Sidebar() {
+
+const menuList=[
+  {
+    group:"General",
+    items:[
+      {
+        link:"/",
+        icon:<User />,
+        text:"Profile"
+      },
+      {
+        link:"/",
+        icon:<Inbox />,
+        text:"Inbox"
+      },
+      {
+        link:"/",
+        icon:<Receipt />,
+        text:"Billing"
+      },
+      {
+        link:"/",
+        icon:<Bell />,
+        text:"Notifications"
+      }
+      
+    ]
+  },
+  {
+    group:"Settings",
+    items:[
+      {
+        link:"/",
+        icon:<Settings />,
+        text:"General Settings"
+      },
+      {
+        link:"/",
+        icon:<Cookie />,
+        text:"Privacy"
+      },
+      {
+        link:"/",
+        icon:<MessageSquare />,
+        text:"Logs"
+      }
+      
+    ]
+  }
+]
+
+    return (
+      <div className=" flex flex-col gap-2 w-[300px] min-w-[300px] border-r min-h-screen p-4">
+        <div><Useritem /></div>
+        <div className="grow">
+                  <Command style={{overflow:'visible'}}>
+            
+            <CommandList style={{overflow:'visible'}}>
+              {menuList.map((menu:any,key:number)=>(
+              <CommandGroup key={key} heading={menu.group}>
+               {menu.items.map((option:any,optionKey:number)=><CommandItem key={optionKey} className="flex gap-2 cursor-pointer">
+                {option.icon}
+                {option.text}</CommandItem>)}
+              </CommandGroup>))}
+             
+            </CommandList>
+          </Command>
+
+        </div>
+        <div>Settings</div>
+      </div>
+    );
+  }
+  
---
+++ src/components/Useritem.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+export default function Useritem() {
+    return (
+     <div className="flex items-center gap-4 border rounded-full">
+        <div className="avatar rounded-full h-12 w-12 bg-emerald-500 text-white font-[700] flex items-center justify-center">
+            <p>PE</p>
+        </div>
+        <div className="grow">
+        <p className="text-[16px] font-bold">pathii</p>
+        <p className="text-[12px] text-neutral-500">kljl@gmail.com</p>
+        </div>
+       
+        
+     </div>
+    );
+  }
